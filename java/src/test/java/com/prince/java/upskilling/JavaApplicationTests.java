72cd459f4072e5e434e457fbef791a58e58d6b94
Prince <prince@synthesis.co.za>
2024-05-02 18:51:58 +0200

    Added Spingboot Project Structure

---
+++ java/src/test/java/com/prince/java/upskilling/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package java.src.test.java.com.prince.java.upskilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
